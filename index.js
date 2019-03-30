f935034dcae6c3ca373156a46b4f37940f2f0a47
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-30 16:45:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver}
+  newDriver[key] = value;
+
+  return newDriver;
+}
+
+// function nondestructivelyUpdateObject(obj, key, value) {
+//   const newObj = { ...obj };
+//
+//   newObj[key] = value;
+//
+//   return newObj;
+// }
+//
+// const sundayMenu = nondestructivelyUpdateObject(tuesdayMenu, 'fries', 'Shoestring');
+//
+// tuesdayMenu.fries;
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
+}
